681cac8a6586c3cb0ecc974a02438eea5ac9bb34
Julien Flusin <50296610+jul-fls@users.noreply.github.com>
2024-07-01 23:55:17 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import * as Sentry from "@sentry/node";
-// import { nodeProfilingIntegration } from "@sentry/profiling-node";
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
 import express from "express";
 import * as dotevnv from "dotenv";
 import cors from "cors";
@@ -14,17 +14,17 @@ if (!process.env.PORT) {
 
 const PORT = parseInt(process.env.PORT as string, 10);
 
-// Sentry.init({
-//     dsn: "https://164c2998d9f2965f71b50cb1eca47d32@o4507378463080448.ingest.de.sentry.io/4507487337185360",
-//     integrations: [
-//         nodeProfilingIntegration(),
-//     ],
-//     // Performance Monitoring
-//     tracesSampleRate: 1.0, //  Capture 100% of the transactions
+Sentry.init({
+    dsn: "https://164c2998d9f2965f71b50cb1eca47d32@o4507378463080448.ingest.de.sentry.io/4507487337185360",
+    integrations: [
+        nodeProfilingIntegration(),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
 
-//     // Set sampling rate for profiling - this is relative to tracesSampleRate
-//     profilesSampleRate: 1.0,
-// });
+    // Set sampling rate for profiling - this is relative to tracesSampleRate
+    profilesSampleRate: 1.0,
+});
 
 const app = express();
 
